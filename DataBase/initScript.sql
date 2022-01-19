74b950a204a9e4e92d9ab26ef68e1e50fe9362dc
Saber011 <44784003+Saber011@users.noreply.github.com>
2022-01-20 02:04:02 +0500

    Update initScript.sql

--- DataBase/initScript.sql
+++ DataBase/initScript.sql
@@ -1,281 +1,222 @@
 create schema core;
 
-create table core.article
+create table if not exists core.article
 (
-	article_id serial
+	article_id serial not null
 		constraint article_pk
 			primary key,
 	text text not null
 );
 
-create table core.role
+alter table core.article owner to postgres;
+
+create table if not exists core.role
 (
-	role_id int
+	role_id integer not null
 		constraint role_pk
 			primary key,
 	name varchar(50) not null
 );
 
-create unique index role_name_uindex
+alter table core.role owner to postgres;
+
+create unique index if not exists role_name_uindex
 	on core.role (name);
 
-create table core.user
+create table if not exists core."user"
 (
-	user_id int
+	user_id serial not null
 		constraint user_pk
 			primary key,
 	login varchar(120) not null,
-	password text not null
+	password text not null,
+	is_deleted integer
 );
 
-create unique index user_login_uindex
-	on core.user (login);
+alter table core."user" owner to postgres;
 
-create table core.category
+create unique index if not exists user_login_uindex
+	on core."user" (login);
+
+create table if not exists core.category
 (
-	category_id serial
+	category_id serial not null
 		constraint category_pk
 			primary key,
 	name varchar(120) not null
 );
 
-create unique index category_name_uindex
+alter table core.category owner to postgres;
+
+create unique index if not exists category_name_uindex
 	on core.category (name);
 
-
-create table core.test
+create table if not exists core.test
 (
-	test_id serial
+	test_id serial not null
 		constraint test_pk
 			primary key,
 	name varchar(120) not null,
-	category_id int
+	category_id integer
 		constraint test_category_category_id_fk
 			references core.category
 );
 
-create table core.test_type
+alter table core.test owner to postgres;
+
+create table if not exists core.test_type
 (
-	test_type_id serial
+	test_type_id serial not null
 		constraint test_type_pk
 			primary key,
 	name varchar(120) not null
 );
 
-create unique index test_type_name_uindex
+alter table core.test_type owner to postgres;
+
+create unique index if not exists test_type_name_uindex
 	on core.test_type (name);
 
-create table core.test_task
+create table if not exists core.test_task
 (
-	test_task_id serial
+	test_task_id serial not null
 		constraint test_task_pk
 			primary key,
 	description text not null,
-	test_id int not null
+	test_id integer not null
 		constraint test_task_test_test_id_fk
 			references core.test,
-	answers text[],
-	response_options text[],
-	test_type_id int
+	test_type_id integer
 		constraint test_task_test_type_test_type_id_fk
 			references core.test_type
 );
 
+alter table core.test_task owner to postgres;
 
-create table core.ref_test_category
+create table if not exists core.ref_test_category
 (
-	test_id int
+	test_id integer
 		constraint ref_test_category_test_test_id_fk
 			references core.test,
-	category_id int
+	category_id integer
 		constraint ref_test_category_category_category_id_fk
 			references core.category
 );
 
-create table core.ref_article_category
+alter table core.ref_test_category owner to postgres;
+
+create table if not exists core.ref_article_category
 (
-	article_id int
+	article_id integer
 		constraint ref_article_category_article_article_id_fk
 			references core.article,
-	category_id int
+	category_id integer
 		constraint ref_article_category_category_category_id_fk
 			references core.category
 );
 
-create table core.user_roles
+alter table core.ref_article_category owner to postgres;
+
+create table if not exists core.user_roles
 (
-	user_id int
+	user_id integer
 		constraint user_roles_user_user_id_fk
 			references core."user",
-	role_id int
+	role_id integer
 		constraint user_roles_role_role_id_fk
 			references core.role
 );
 
-create table core.test_result
-(
-	test_result_id serial
-		constraint test_result_pk
-			primary key,
-	user_id int
-		constraint test_result_user_user_id_fk
-			references core."user",
-	test_id int
-		constraint test_result_test_test_id_fk
-			references core.test,
-	correct_answers text[],
-	incorrect_answers text[]
-);
+alter table core.user_roles owner to postgres;
 
-create table core.user_authorization_provider
+create table if not exists core.user_authorization_provider
 (
-	user_authorization_provider_id serial
+	user_authorization_provider_id serial not null
 		constraint user_authorization_provider_pk
 			primary key,
-	user_id int
+	user_id integer
 		constraint user_authorization_provider_user_user_id_fk
 			references core."user",
 	token text
 );
 
-create sequence core.user_user_id_seq;
+alter table core.user_authorization_provider owner to postgres;
 
-alter table core."user" alter column user_id set default nextval('core.user_user_id_seq');
-
-alter sequence core.user_user_id_seq owned by core."user".user_id;
-
-
-create table core."user_token"
+create table if not exists core.user_token
 (
-	user_id int
-		constraint "user_token_user_user_id_fk"
+	user_id integer
+		constraint user_token_user_user_id_fk
 			references core."user",
-	token text
+	token text,
+	expires date,
+	created date,
+	created_by_ip text,
+	revoked date,
+	revoked_by_ip text,
+	replaced_by_token text
 );
 
-alter table core.user_token
-	add expires date;
+alter table core.user_token owner to postgres;
 
-alter table core.user_token
-	add created date;
-
-alter table core.user_token
-	add created_by_ip text;
-
-alter table core.user_token
-	add revoked date;
-
-alter table core.user_token
-	add revoked_by_ip text;
-
-alter table core.user_token
-	add replaced_by_token text;
-
-create table core.test_task_response_option
+create table if not exists core.test_task_response_option
 (
-	test_task_response_option serial
+	test_task_response_option_id serial not null
 		constraint test_task_response_option_pk
 			primary key,
 	response_option text,
-	test_task_id int
+	test_task_id integer
 		constraint test_task_response_option_test_task_test_task_id_fk
 			references core.test_task
 );
 
-alter table core.test_task_response_option rename column test_task_response_option to test_task_response_option_id;
+alter table core.test_task_response_option owner to postgres;
 
-alter table core.test_task rename column response_options to test_task_response_option_id;
-
---alter table core.test_task alter column test_task_response_option_id type int using test_task_response_option_id::int;
-
--- alter table core.test_task
--- 	add constraint test_task_test_task_response_option_test_task_response_option_id_fk
--- 		foreign key (test_task_response_option_id) references core.test_task_response_option;
-
-create table core.test_task_answer
+create table if not exists core.test_result
 (
-	test_task_answer_id int
+	test_result_id serial not null
+		constraint test_result_pk
+			primary key,
+	user_id integer
+		constraint test_result_user_user_id_fk
+			references core."user",
+	test_id integer
+		constraint test_result_test_test_id_fk
+			references core.test,
+	test_task_response_option integer
+		constraint test_result_test_task_response_option_test_task_response_option
+			references core.test_task_response_option
+);
+
+alter table core.test_result owner to postgres;
+
+create table if not exists core.test_task_answer
+(
+	test_task_answer_id serial not null
 		constraint test_task_answer_pk
 			primary key,
-	test_task_id int
+	test_task_id integer
 		constraint test_task_answer_test_task_test_task_id_fk
 			references core.test_task,
 	answer text
 );
 
-alter table core.test_task rename column answers to test_task_answer_id;
+alter table core.test_task_answer owner to postgres;
 
---alter table core.test_task alter column test_task_answer_id type integer using test_task_answer_id::integer;
-
---alter table core.test_task alter column test_task_response_option_id type integer using test_task_response_option_id::integer;
-
--- alter table core.test_task
--- 	add constraint test_task_test_task_answer_test_task_answer_id_fk
--- 		foreign key (test_task_answer_id) references core.test_task_answer;
-
--- alter table core.test_task
--- 	add constraint test_task_test_task_response_option_test_task_response_option_id_fk
--- 		foreign key (test_task_response_option_id) references core.test_task_response_option;
-
-alter table core.test_result rename column correct_answers to correct_answers_id;
-
--- alter table core.test_result alter column correct_answers_id type integer using correct_answers_id::integer;
-
-alter table core.test_result drop column incorrect_answers;
-
--- alter table core.test_result
--- 	add constraint test_result_test_task_answer_test_task_answer_id_fk
--- 		foreign key (correct_answers_id) references core.test_task_answer;
-
-alter table core.test_task drop column test_task_answer_id;
-
-alter table core.test_task
-	add test_task_answer_id integer;
-
-alter table core.test_task drop column test_task_response_option_id;
-
-alter table core.test_task
-	add test_task_response_option_id integer;
-
-alter table core.test_task
-	add constraint test_task_test_task_answer_test_task_answer_id_fk
-		foreign key (test_task_answer_id) references core.test_task_answer;
-
-alter table core.test_task
-	add constraint test_task_test_task_response_option_test_task_response_option_id_fk
-		foreign key (test_task_response_option_id) references core.test_task_response_option;
-
-alter table core.test_result drop column correct_answers_id;
-alter table core.test_result
-	add correct_answers_id integer;
-
--- alter table core.test_result drop column incorrect_answers;
-
-alter table core.test_result
-	add constraint test_result_test_task_answer_test_task_answer_id_fk
-		foreign key (correct_answers_id) references core.test_task_answer;
-
-alter table core.test_result rename column correct_answers_id to test_task_response_option;
-
-alter table core.test_result drop constraint test_result_test_task_answer_test_task_answer_id_fk;
-
-alter table core.test_result
-	add constraint test_result_test_task_response_option_test_task_response_option_id_fk
-		foreign key (test_task_response_option) references core.test_task_response_option;
-create table core.dictionary
+create table if not exists core.dictionary
 (
-    dictionary_id   serial       not null
-        constraint dictionary_pk
-            primary key,
-    dictionary_name varchar(120) not null,
-    table_name      varchar(120) not null
+	dictionary_id serial not null
+		constraint dictionary_pk
+			primary key,
+	dictionary_name varchar(120) not null,
+	table_name varchar(120) not null
 );
 
-alter table core.dictionary
-    owner to postgres;
+alter table core.dictionary owner to postgres;
 
-create unique index dictionary_dictionary_name_uindex
-    on core.dictionary (dictionary_name);
+create unique index if not exists core.dictionary_dictionary_name_uindex
+	on core.dictionary (dictionary_name);
+
+
 
 INSERT INTO core.dictionary (dictionary_id, dictionary_name, table_name) VALUES (DEFAULT, 'Категории', 'category');
 INSERT INTO core.dictionary (dictionary_id, dictionary_name, table_name) VALUES (DEFAULT, 'Роли', 'role');
